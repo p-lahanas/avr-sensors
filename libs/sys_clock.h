8a0dac8956245e3b9e932a412f581b1ad7834350
p-lahanas <plahanas18@gmail.com>
2022-03-03 14:19:38 +1000

    Started clock handling

---
+++ libs/sys_clock.h
@@ -0,0 +1,8 @@
+#ifndef SYS_CLOCK_H
+#define SYS_CLOCK_H
+
+
+void sys_time_init(void);
+unsigned long sys_time_elapsed(void);
+
+#endif
--- programs/main.c
+++ programs/main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "cli_lib.h"
+#include "sys_clock.h"
 
 #include <util/delay.h>
 
@@ -9,12 +10,14 @@ int main() {
     USART_Init(UBRR_9600_BAUD);
 
     unsigned char hello[14] = "Hello World!\n\r";
+    sys_time_init();
+
+    unsigned long time;
 
     while (1) {
-
-        USART_TransmitStrPolling(hello, 14);
+        time = sys_time_elapsed();
+        USART_TransmitPolling(time + 'a');
         _delay_ms(100);
-
     }
     
     return 0;
